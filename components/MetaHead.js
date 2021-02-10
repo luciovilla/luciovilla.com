a371d0f448fcba24e627e241fe31cd4190d34b46
Lucio Villa <lucio310@gmail.com>
2021-02-10 16:36:44 -0500

    update twitter card

--- components/MetaHead.js
+++ components/MetaHead.js
@@ -37,7 +37,7 @@ export default function MetaHead() {
       <meta property="og:title" content={pageTitle} />
       <meta property="og:description" content={pageDescription} />
       <meta name="twitter:site" content="@luciovilla" />
-      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={pageTitle} />
       <meta name="twitter:description" content={pageDescription} />
       <meta name="twitter:image" content="/site.png" />
