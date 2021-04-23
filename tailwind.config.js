4d74e6514b867743da1675944c276eed32ad47cf
Lucio Villa <lucio310@gmail.com>
2021-04-23 18:08:58 -0400

    add new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
+const { spacing, fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans],
+      },
+    },
     minHeight: {
       0: '0',
       '1/4': '25vh',
       '1/2': '50vh',
       '3/4': '75vh',
-      65:'65vh',
+      65: '65vh',
       80: '80vh',
       85: '85vh',
       full: '100vh',
