9266948749ed5b11891491aaf5130e5a5e3442cc
Lucio Villa <lucio310@gmail.com>
2021-04-23 19:03:38 -0400

    add link to source code in footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,16 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className="text-xs py-6 text-gray-600 border-t w-full text-center">
-      Site built with Next.js, React and Tailwind CSS.
+      Site built with Next.js and Tailwind CSS. View{' '}
+      <a
+        href="https://github.com/luciovilla/new-newsite"
+        className="underline"
+        target="_blank"
+        rel="noopener"
+      >
+        source
+      </a>
+      .
     </footer>
   )
 }
