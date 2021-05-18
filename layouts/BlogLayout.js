fc2af217459c6f38a6ddaca6259de2cacaf3e361
Lucio Villa <lucio310@gmail.com>
2021-05-18 00:48:44 -0400

    update blog meta title

--- layouts/BlogLayout.js
+++ layouts/BlogLayout.js
@@ -3,7 +3,7 @@ import Container from '../components/Container'
 export default function BlogLayout({ children, data }) {
   return (
     <Container
-      title={data.properties.Post.title[0].plain_text}
+      title={`${data.properties.Post.title[0].plain_text} – Lucio Villa`}
       description={data.properties.Description.rich_text[0].plain_text}
       date={new Date(data.properties.Date.date.start)}
       type="article"
