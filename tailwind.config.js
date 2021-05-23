bcb1cd6b797f88b8be8ea1ff4171b4312f70324f
Lucio Villa <lucio310@gmail.com>
2021-05-23 14:10:42 -0400

    enable JIT in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,18 @@
 const { fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
+  purge: ['./pages/**/*.js', './components/**/*.js'],
   theme: {
+    fontFamily: {
+      body: ['Inter', ...fontFamily.sans],
+      sans: ['Inter', ...fontFamily.sans],
+    },
     extend: {
-      fontFamily: {
-        sans: ['Inter', ...fontFamily.sans],
+      colors: {
+        blue: '#01417b',
+        yellow: '#ffa70a',
       },
     },
-    minHeight: {
-      0: '0',
-      '1/4': '25vh',
-      '1/2': '50vh',
-      '3/4': '75vh',
-      65: '65vh',
-      75: '75vh',
-      80: '80vh',
-      85: '85vh',
-      full: '100vh',
-    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
