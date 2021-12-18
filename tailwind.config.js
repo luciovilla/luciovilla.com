97f0ff6fde8e277ff5b0d65a11b41fceb5250eb1
Lucio Villa <lucio310@gmail.com>
2021-12-18 11:28:19 -0500

    upgrade tailwind and remove unused pkgs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 const { fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.tsx', './components/**/*.tsx', './layouts/**/*.tsx'],
+  content: ['./pages/**/*.tsx', './components/**/*.tsx', './layouts/**/*.tsx'],
   theme: {
     fontFamily: {
       body: ['Inter', ...fontFamily.sans],
