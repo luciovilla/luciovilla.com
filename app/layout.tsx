028102244c4658c9ab0a6817e305f26df232dd2c
Lucio Villa <lucio310@gmail.com>
2023-02-06 02:39:08 -0500

    moving script tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,24 +8,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=UA-28929069-1"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-28929069-1');
-        `}
-        </Script>
         <Nav />
         <main id="skip" className="px-8">
           {children}
         </main>
         <Footer />
       </body>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=UA-28929069-1" />
+      <Script id="google-analytics">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-28929069-1');
+        `}
+      </Script>
     </html>
   )
 }
