10f94137545031e704bb391c99437b1a776e2d7d
Lucio Villa <lucio310@gmail.com>
2023-02-11 12:43:08 -0500

    move to ga4

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,13 +14,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         </main>
         <Footer />
       </body>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=UA-28929069-1" />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-JDMZNFHMVW" />
       <Script id="google-analytics">
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'UA-28929069-1');
+            gtag('config', 'G-JDMZNFHMVW');
         `}
       </Script>
     </html>
