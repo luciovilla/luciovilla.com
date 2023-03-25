41a757af4fb4edf79b31122de7b50171fec80ffa
Lucio Villa <lucio310@gmail.com>
2023-03-25 18:03:47 -0400

    add robots.txt and sitemap.xml

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import { getNotionData } from '@lib/getNotionData'
+import { PostType } from '@lib/types'
+
+export default async function sitemap() {
+  const postData: Object[] = await getNotionData()
+
+  const notas = postData.map((nota: PostType) => ({
+    url: `https://luciovilla.com/notas/${nota.properties.Slug.rich_text[0].plain_text}`,
+    lastModified: nota.last_edited_time
+  }))
+
+  const routes = ['', '/about-me', '/notas', '/projects'].map((route) => ({
+    url: `https://luciovilla.com${route}`,
+    lastModified: new Date().toISOString()
+  }))
+
+  return [...routes, ...notas]
+}
