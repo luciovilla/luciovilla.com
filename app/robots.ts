002100bc091a4428872b8a21758597f05b98c086
Lucio Villa <lucio310@gmail.com>
2023-03-25 18:06:07 -0400

    add robots.txt and sitemap.xml (#8)

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*'
+      }
+    ],
+    sitemap: 'https://luciovilla.com/sitemap.xml',
+    host: 'https://luciovilla.com'
+  }
+}
